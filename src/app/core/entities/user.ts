6527fa5a65e2ca85bfb56a47d59f0b302edcd2cf
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-05 13:43:30 -0300

    📦 FEAT: userEntity implemented

---
+++ src/app/core/entities/user.ts
@@ -0,0 +1,60 @@
+import { Either, Left, Right } from "../errors/either"
+import { ErrorBase } from "../errors/errorBase"
+import { EmailInvalidError } from "../errors/user/emailInvalid"
+import { NameInvalidError } from "../errors/user/nameInvalid"
+import { PasswordInvalidError } from "../errors/user/passwordInvalid"
+
+export interface UserInterface{
+    id:string
+    createdAt:Date,
+    updatedAt:Date,
+    name:string,
+    email:string,
+    password:string
+}
+
+
+export class UserEntity{
+    user:UserInterface
+
+    constructor(user:UserInterface){
+        this.user = user
+    }
+
+    isValidPassword(): boolean {
+        if(!this.user.password) return false
+        if (this.user.password.length < 8 || this.user.password.length > 15) {
+          return false; 
+        }
+        if (!/[A-Z]/.test(this.user.password)) {
+          return false;
+        }
+        return true;
+    }
+
+    isValidEmail(): boolean {
+        if(!this.user.email) return false
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(this.user.email);
+    }
+
+    isValidName(): boolean {
+        if(!this.user.name) return false
+        return this.user.name.length >= 4 && this.user.name.length <= 60;
+    }
+
+    static create(userData:Omit<UserInterface,"id"|"createdAt"|"updatedAt">):Either<ErrorBase,UserEntity>{
+        const user = new UserEntity({
+            ...userData,
+            id:"",
+            createdAt: new Date(),
+            updatedAt: new Date()
+        })
+
+        if(!user.isValidEmail()) return Left.create(new EmailInvalidError())  
+        if(!user.isValidName()) return Left.create(new NameInvalidError())      
+        if(!user.isValidPassword()) return Left.create(new PasswordInvalidError())      
+
+        return Right.create(user)
+    }
+}
