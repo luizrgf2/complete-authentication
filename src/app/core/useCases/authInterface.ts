2afac9895a96a20b5aadb880288e591fefdda9ad
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-05 13:41:27 -0300

    📦 FEAT: contract AuthUseCaseInterface created

---
+++ src/app/core/useCases/authInterface.ts
@@ -0,0 +1,15 @@
+import { UserInterface } from "../entities/user";
+import { Either } from "../errors/either";
+import { ErrorBase } from "../errors/errorBase";
+
+
+export interface AuthUseCaseInput extends Omit<UserInterface,"id"|"createdAt"|"updatedAT"|"name">{}
+
+export interface AuthUseCaseOutput{
+    user:UserInterface,
+    token:string
+}
+
+export interface AuthUseCaseInterface{
+    exec:(input:AuthUseCaseInput)=>Promise<Either<ErrorBase,AuthUseCaseOutput>>
+}
