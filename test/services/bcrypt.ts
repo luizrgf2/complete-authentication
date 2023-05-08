27d95d2440d8c82cbab787a9ebe0122d5aa5fc20
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-08 20:41:07 -0300

    📦 FEAT: BcryptInMemory implemented

---
+++ test/services/bcrypt.ts
@@ -0,0 +1,13 @@
+import { Either, Right } from "../../src/app/core/errors/either";
+import { ErrorBase } from "../../src/app/core/errors/errorBase";
+import { BcryptInterface } from "../../src/app/data/interfaces/bcrypt";
+
+export class BcryptInMemory implements BcryptInterface{
+    async hash(password: string): Promise<Either<ErrorBase, string>> {
+        return Right.create("password encypted")
+    }
+    async compare(password: string, hash: string): Promise<Either<ErrorBase, boolean>> {
+        return Right.create(true)
+    }
+    
+}
