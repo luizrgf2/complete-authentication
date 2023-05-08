569da3747c3f330ef337ac83faf4c09aadfb8ec8
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-08 12:15:47 -0300

    📦 FEAT: AuthUseCaseInterface implemented

--- src/app/core/useCases/registerInterface.ts
+++ src/app/core/useCases/registerInterface.ts
@@ -3,12 +3,12 @@ import { Either } from "../errors/either";
 import { ErrorBase } from "../errors/errorBase";
 
 
-export interface RegisterUseCaseInput extends Omit<UserInterface,"id"|"createdAt"|"updatedAT">{}
+export interface RegisterUseCaseInput extends Omit<UserInterface,"id"|"createdAt"|"updatedAt">{}
 
 export interface RegisterUseCaseOutput{
     user:UserInterface
 }
 
-export interface AuthUseCaseInterface{
+export interface RegisterUseCaseInterface{
     exec:(input:RegisterUseCaseInput)=>Promise<Either<ErrorBase,RegisterUseCaseOutput>>
 }
