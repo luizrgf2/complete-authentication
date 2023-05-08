be741935886553e6aacd7ee56558a2b57da00b9d
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-08 12:13:48 -0300

    📦 FEAT: UserRepositoryInterface implemented

---
+++ src/app/data/interfaces/repository/user.ts
@@ -0,0 +1,9 @@
+import { UserInterface } from "../../../core/entities/user";
+import { Either } from "../../../core/errors/either";
+import { ErrorBase } from "../../../core/errors/errorBase";
+
+export interface UserRepositoryInterface{
+    create:(user:Omit<UserInterface,"id"|"createdAt"|"updatedAt">)=>Promise<Either<ErrorBase,UserInterface>>
+    findByEmail:(email:string)=>Promise<Either<ErrorBase,UserInterface>>
+    findById:(id:string)=>Promise<Either<ErrorBase,UserInterface>>
+}
