5c2f90b49ae88519248215791e6aff1f55e98cc0
Luiz Ricardo <luizrgfelipe@gmail.com>
2023-05-11 17:40:28 -0300

    ✅ TEST: add new users to test

--- test/mocks/user/user.ts
+++ test/mocks/user/user.ts
@@ -3,39 +3,60 @@ import { UserInterface } from "../../../src/app/core/entities/user";
 export const  UserValid =  {
     email:"teste@gmail.com",
     password:"Teste123",
-    name:"Felipe Ramos"
+    name:"Felipe Ramos",
+    accountConfirmed:true
+} as UserInterface
+
+
+export const  UserWithConfirmationFalse=  {
+    email:"teste@gmail.com",
+    password:"Teste123",
+    name:"Felipe Ramos",
+    accountConfirmed:false
+} as UserInterface
+
+export const  UserWithoutConfirmation=  {
+    email:"teste@gmail.com",
+    password:"Teste123",
+    name:"Felipe Ramos",
 } as UserInterface
 
 export const  UserWithInvalidEmail =  {
     email:"testegmail.com",
     password:"Teste123",
-    name:"Felipe Ramos"
+    name:"Felipe Ramos",
+    accountConfirmed:true
 } as UserInterface
 
 export const  UserWithInvalidPassword =  {
     email:"teste@gmail.com",
     password:"Teste",
-    name:"Felipe Ramos"
+    name:"Felipe Ramos",
+    accountConfirmed:true
 } as UserInterface
 
 export const  UserWithInvalidName =  {
     email:"teste@gmail.com",
     password:"Teste123",
-    name:"Fel"
+    name:"Fel",
+    accountConfirmed:true
 } as UserInterface
 
 export const  UserWithWithoutName =  {
     email:"teste@gmail.com",
     password:"Teste123",
+    accountConfirmed:true
 } as UserInterface
 
 export const  UserWithWithoutEmail =  {
     password:"Teste123",
-    name:"Luiz Fellipe"
+    name:"Luiz Fellipe",
+    accountConfirmed:true
 } as UserInterface
 
 export const  UserWithoutPassword =  {
     email:"teste@gmail.com",
-    name:"Luiz Fellipe"
+    name:"Luiz Fellipe",
+    accountConfirmed:true
 } as UserInterface
 
